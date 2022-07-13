046740200c5db05ef77823c7a8ff8b7f55ebbdb2
Sarah Clark <smarieclark901@gmail.com>
2022-07-13 11:33:52 -0400

    update portfolio to fit my vision

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log("Welcome to my portfolio!");
+// Function that opens the menu when it is closed, and closes the menu when it is open
+function menuToggle() {
+  var x = document.getElementById('mynavtoggle');
+  // Below changes class name based on existing class name, so that the proper version of the menu (shown or hidden) will be displayed.
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
